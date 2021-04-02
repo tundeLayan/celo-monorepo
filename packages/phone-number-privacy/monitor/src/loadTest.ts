e27b9a23404ba532e46c7f0e5af44ca33a9e12c9
codyborn <codyborn@outlook.com>
2021-04-01 20:13:59 -0700

    Run 10 at a time

--- packages/phone-number-privacy/monitor/src/loadTest.ts
+++ packages/phone-number-privacy/monitor/src/loadTest.ts
@@ -1,24 +1,28 @@
-import { PhoneNumberHashDetails } from '@celo/identity/lib/odis/phone-number-identifier'
 import { ErrorMessages } from '@celo/identity/lib/odis/query'
 import { rootLogger as logger } from '@celo/phone-number-privacy-common'
 import { queryOdisForSalt } from './query'
 
 export const runTest = async () => {
-  logger.info('Performing test query')
-  try {
-    const odisResponse: PhoneNumberHashDetails = await queryOdisForSalt()
-    logger.info({ odisResponse }, 'ODIS salt request successful. System is healthy.')
-  } catch (err) {
-    if ((err as Error).message === ErrorMessages.ODIS_QUOTA_ERROR) {
-      logger.info(
-        { error: err },
-        'ODIS salt request out of quota. This is expected. System is healthy.'
+  while (true) {
+    const test = []
+    for (let i = 0; i < 10; i++) {
+      test.push(
+        new Promise(async (resolve, reject) => {
+          logger.info('Performing test query')
+          try {
+            await queryOdisForSalt()
+            resolve(true)
+          } catch (err) {
+            if ((err as Error).message === ErrorMessages.ODIS_QUOTA_ERROR) {
+              resolve(true)
+            } else {
+              reject(err)
+            }
+          }
+        })
       )
-    } else {
-      logger.error('ODIS salt request failed.')
-      logger.error({ err })
-      throw err
     }
+    await Promise.all(test)
   }
 }
 
