7c9fde2487873cb85563d138981b08ce225be0e2
Emil Hotkowski <emil.hotkowski@gmail.com>
2021-07-29 15:43:37 +0200

    Added GCloud init

--- packages/phone-number-privacy/release/deployment.sh
+++ packages/phone-number-privacy/release/deployment.sh
@@ -1,3 +1,4 @@
 echo "Releasing ODIS to $ENV_NAME!"
+gcloud init --console-only
 yarn keys:decrypt
 ./packages/celotool/bin/celotooljs.sh deploy upgrade odis --celoEnv $NETWORK --context azure-odis-eastus-1
