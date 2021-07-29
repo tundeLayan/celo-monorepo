eeba2c10490403bd3d5f47533e47e39db0847c87
Emil Hotkowski <emil.hotkowski@gmail.com>
2021-07-29 15:51:26 +0200

    Exact commint for safety

--- packages/phone-number-privacy/release/deployment.sh
+++ packages/phone-number-privacy/release/deployment.sh
@@ -1,3 +1,3 @@
-echo "Releasing ODIS to $ENV_NAME!"
+echo "Releasing ODIS to $ENV_NAME"
 yarn keys:decrypt
 ./packages/celotool/bin/celotooljs.sh deploy upgrade odis --celoEnv $NETWORK --context azure-odis-eastus-1
